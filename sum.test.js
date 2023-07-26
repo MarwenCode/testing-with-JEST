f848011bb81533c05c0975d8393b6d5f750da022
MarwenCode <marwen_jdidi@yahoo.com>
2023-07-26 23:21:15 +0200

    test numbers & arrays

--- sum.test.js
+++ sum.test.js
@@ -1,15 +1,43 @@
-const sum = require("./sum")
 
- it("should add 1 +2 to equal 3 ", () => {
-    const result = sum(1,2);
-    expect(result).toBe(3)
- })
+const sum = require("./sum");
+const { default: expect } = require("expect");
+
+it("should add 1 +2 to equal 3 ", () => {
+  const result = sum(1, 2);
+  expect(result).toBe(3);
+});
+
+it("null", () => {
+  const n = null;
+  expect(n).toBeFalsy();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeNull();
+});
+
+it("tow plus two", () => {
+  const value = 2 + 2;
+  expect(value).toBe(4);
+});
 
 
- describe("truth or false" , () => {
-    it("null", () => {
-        const n = null;
-        expect(n).toBeFalsy();
-        
+describe("numbers", () =>  { 
+    it("two plus two", () => {
+        const value = 2 + 2;
+        expect(value).toBe(4);
+        expect(value).toBeGreaterThan(3)
     })
- })
+
+    it("adding floats", () => {
+        const value = 0.1 + 0.2;
+        expect(value).toBeCloseTo(0.299995555)
+    })
+})
+
+
+//arrays
+describe("arrays", () => {
+    const shoppingList = [
+        'diappers', "kleenex", 'milk'
+    ]
+    expect(shoppingList).toContain("milk");
+})
